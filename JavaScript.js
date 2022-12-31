0e6068222d61298ac51e2f7f81bc863808d76be3
txbyjs <toby.symons5@gmail.com>
2022-12-31 21:41:27 +0000

    v20 [Revision Website Complete]
    fortnite

--- JavaScript.js
+++ JavaScript.js
@@ -9,3 +9,16 @@ function w3_open() {
     document.getElementById("myOverlay").style.display = "none";
   }
   
+  var myDate = new Date();
+  var hrs = myDate.getHours();
+  
+  var greet;
+  
+  if (hrs < 12)
+    greet = 'Good Morning';
+  else if (hrs >= 12 && hrs <= 17)
+    greet = 'Good Afternoon';
+  else if (hrs >= 17 && hrs <= 24)
+    greet = 'Good Evening';
+  
+  document.getElementById('greetings').innerHTML ='<b>' + greet + '</b>';
